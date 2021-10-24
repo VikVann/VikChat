dbd9ba4c43822bbd6c8bb6e23890182e286a148a
VikVann <92604464+VikVann@users.noreply.github.com>
2021-10-25 04:00:50 +0500

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,72 @@
+import React, {useContext, useState} from 'react';
+import {Context} from "../index";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Avatar, Button, Container, Grid, TextField} from "@mui/material";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import Loader from "./Loader";
+
+const Chat = () => {
+    const {auth, firestore} = useContext(Context)
+    const [user] = useAuthState(auth)
+
+    const [value, setValue] = useState('')
+
+    const [messages, loading, error] = useCollectionData(
+        firestore.collection('messages')
+    )
+
+    const sendMessage = async () => {
+        firestore.collection('messages').add({
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            text: value
+        })
+        setValue('')
+    }
+
+    if(loading) {
+        return <Loader />
+    }
+    console.log(messages)
+    return (
+        <Container>
+            <Grid container
+                  justify={"center"}
+                  style={{height: window.innerHeight - 50, marginTop: 20}}>
+                <div style={{width:'800%', height: '60vh', border: '1px solid gray', overflowY: 'auto'}}>
+                    {messages.map(message =>
+                        <div style={{
+                            margin: 10,
+                            border: user.uid === message.uid ? '1px solid green' : '2px dashed red',
+                            marginLeft: user.uid === message.uid ? 'auto' : '10px',
+                            width: 'fit-content',
+                            padding: 5
+                        }}>
+                            <Grid container>
+                                <Avatar src={message.photoURL} />
+                                <div>{message.displayName}</div>
+                            </Grid>
+                            <div>{message.text}</div>
+                        </div>
+                    )}
+                </div>
+                <Grid container
+                      direction={'column'}
+                      alignItems={"flex-end"}
+                      style={{width:'100%'}}>
+                    <TextField
+                        fullWidth
+                        variant={"outlined"}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}>
+
+                    </TextField>
+                    <Button onClick={sendMessage} variant={"outlined"}>Отправить</Button>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Chat
